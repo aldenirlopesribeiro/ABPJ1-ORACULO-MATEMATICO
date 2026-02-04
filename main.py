51395f101e641b831d22f70d4b1d17fbc7ff7cd7
aldenirlopesribeiro <aldenirlopes17@gmail.com>
2026-02-03 21:37:59 -0300

    Add monthly expense calculation functionality

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+renda_mensal=float(input("digite o valor da renda mensal"))
+despesa_medicamentos=float(input("digite o valor das despesas com medicamentos"))
+despesa_de_aluguel=float(input("digite o valor do aluguel"))
+renda_variavel=float(input("digite o valor da renda extra"))
+
+calculo=renda_mensal-despesa_medicamentos-despesa_de_aluguel+renda_variavel
+
+print(f"calculo despesas mensal{calculo}")
