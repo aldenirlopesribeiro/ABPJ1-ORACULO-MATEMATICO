92e349f6658396a1ebfd82bc59c2cd45a6231cb2
aldenirlopesribeiro <aldenirlopes17@gmail.com>
2026-02-05 19:26:20 -0300

    Add comments for financial control program
    Added comments explaining the purpose of the program and its output.

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ renda_variavel=float(input("digite o valor da renda extra"))
 calculo=renda_mensal-despesa_medicamentos-despesa_de_aluguel+renda_variavel
 
 print(f"calculo despesas mensal{calculo}")
+#esse programa ajuda o usuario a ter um controle financeiro sobre renda e despesas.
+#o calculo final deve traser o resultado financeiro do mes, se positivo ou negativo.
